c6b21a021bffcce09f01ddddabe38d127e8e8aae
Alan(Minkyu Cho) <mrnoname@coupang.com>
2015-07-09 12:55:01 -0700

    Apply Solr Synonyms Module

--- test/test.index.js
+++ test/test.index.js
@@ -1,21 +1,17 @@
-var expect = require('chai').expect;
-var Synonymizer = require('../lib');
-var synonymizer = new Synonymizer(__dirname + '/dictionary.csv');
+var expect = require("chai").expect;
+var Synonymizer = require("../lib");
+var synonymizer = new Synonymizer(__dirname + "/dictionary.csv");
 
-describe('Synonymizer', function() {
-    describe('#synonymize()', function () {
-        it('should return synonymized words from one word', function (done) {
-            synonymizer.synonymize('APC', function(err, result){
-                expect(result).to.equal('아페세');
-                done();
-            });
+describe("Synonymizer", function() {
+    describe("#synonymize()", function () {
+        it("should return synonymized words from one word", function () {
+            var actual = synonymizer.synonymize("APC");
+            expect(actual).to.equal("아페세");
         });
 
-        it('should return synonymized words from words with whitespace', function (done) {
-            synonymizer.synonymize('에잇 세컨', function(err, result){
-                expect(result).to.equal('에잇세컨즈');
-                done();
-            });
+        it("should return synonymized words from words with whitespace", function () {
+            var actual = synonymizer.synonymize("에잇 세컨 청바지");
+            expect(actual).to.equal("에잇세컨즈 청바지");
         });
     });
 });
